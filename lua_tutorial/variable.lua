01cb40f2d6f34c908c89438f60a775fe9d0d1638
Jason Heywood <jasonzhansen@Foxmail.com>
2021-01-16 16:45:37 +0800

    add nil test

--- lua_tutorial/variable.lua
+++ lua_tutorial/variable.lua
@@ -1,7 +1,12 @@
-local tempTable = {}
+-- local tempTable = {}
 
-if 3 == 3 then
-    tempTable.pick = 3
+-- if 3 == 3 then
+--     tempTable.pick = 3
+-- end
+
+-- print(tempTable.pick)
+if nil == 3 then
+    print("ok")
+else
+    print("not ok")
 end
-
-print(tempTable.pick)
