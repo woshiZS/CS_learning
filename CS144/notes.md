6d2f00112478d9dbc023f7866f0a84a7b254e0e7
Jason Heywood <jasonzhansen@Foxmail.com>
2021-02-28 17:28:55 +0800

    finished cs144 Lab0

---
+++ CS144/notes.md
@@ -0,0 +1,17 @@
+# CS144 Lab笔记
+
+## Lab0 Warming up
+
+前面在shell的一些小实验就不写了，自己读manual手册就能收获很多。
+
+### class文档阅读
+
+> std::numeric_limits<template_type>
+>
+> 相当于给出指定数据类型的一些限制，比如最大最小值，能表示的绝对值最小的值。
+>
+> std:string:data()
+>
+> 和c_str()差不多
+
+所需要的接口文档里面都有，唯一需要注意的就是HTTP请求最后一行之后还有一个空行。
--- MIPS/anotherLab4.asm
+++ MIPS/anotherLab4.asm
@@ -1,9 +1,90 @@
+##################################################################################################################
+# Created by:   Mingze, Guo
+#               mguo12
+#               24 Feburary 2021
+#
+# Assignment:   Lab 4: Syntax Checker
+#               CSE 012, Winter 2021
+#
+# Pseudo Code:
+#       int main(int argc, char** argv){
+#           char* firstArg = argv[0]
+#           print("%s\n\n", firstArg)
+#           char c = firstArg[0] 
+#           if (c <= 'A' or ( c > 'Z' and c < 'a') or c > 'z' ){
+#               print("ERROR: Invalid program argument.\n")
+#               return -1
+#           }
+#           if (strlen(firstArg) > 20){
+#               print("ERROR: Invalid program argument.")
+#               return -1
+#           }
+#           int i = 0
+#           int matchedPairs = 0
+#           char* stack = (char *)malloc(sizeof(char)*strlen(firstArg))
+#           char* stackBak = stack
+#           while((c=firstArg[i])!='\0'){
+#               if (c == '(' or c == '[' or c == '{'){
+#                   stack[0] = c
+#                   stack += 1
+#               }
+#               if(c == ')'){
+#                   if(stack[-1] == '('){
+#                   stack -= 1
+#                   matchedPairs += 1
+#                   }
+#                   else{
+#                       print("ERROR - There is a brace mismatch: %c at index %d", c, i)
+#                       return -1
+#                   }
+#               }
+#               if(c == ']'){
+#                   if(stack[-1] == '['){
+#                   stack -= 1
+#                   matchedPairs += 1
+#                   }
+#                   else{
+#                       print("ERROR - There is a brace mismatch: %c at index %d", c, i)
+#                       return -1
+#                   }
+#               }
+#               if(c == '}'){
+#                   if(stack[-1] == '{'){
+#                   stack -= 1
+#                   matchedPairs += 1
+#                   }
+#                   else{
+#                       print("ERROR - There is a brace mismatch: %c at index %d", c, i)
+#                       return -1
+#                   }
+#               }
+#               stack += 1
+#               i += 1
+#           }
+#           if (stack != stackBak){
+#               printf("ERROR - Brace(s) still on stack: ")
+#               while(stack != stackBak){
+#                   print(stack[0])
+#                   stack -= 1
+#               }
+#               print("\n")
+#               return -1
+#           }
+#           print("SUCCESS: There are %d pairs of braces.", matchedPairs)
+#           return 0
+#         }
+#
+#################################################################################################################################
 .data
     content: .space 128
     contLen: .word 127
     prompt: .asciiz "You entered the file:\n"
     badarg: .asciiz "ERROR: Invalid program argument."
-
+    matcherrorpre: .asciiz "ERROR - There is a brace mismatch: "
+    matcherrorsuff: .asciiz " at index "
+    extraerror: .asciiz "ERROR - Brace(s) still on stack: "
+    successpre: .asciiz "SUCCESS: There are "
+    successsuff: .asciiz " pairs of braces."
 
 .text
 main:
@@ -14,6 +95,8 @@ main:
     lw $a0, 0($a1)
     li $v0, 4
     syscall         # Output prompt message
+    jal printNewLine
+    jal printNewLine
 
     # check process
     lw $t0, 0($a1)
@@ -25,8 +108,8 @@ main:
     beqz $t0, badArg
 
     # Read file content
-    move $t1, $a0
-    lw $t0, 0($a1)
+    move $t1, $a1
+    lw $a0, 0($a1)
     li $a1, 0
     li $v0, 13
     syscall
@@ -37,6 +120,54 @@ main:
     li $v0, 14
     syscall
 
+    move $a1, $t1
+    move $t0, $v0
+
+    # allocate a same size memory and record the start address
+    move $a0, $t0
+    li $v0, 9
+    syscall
+
+    move $t7, $v0        # saved for later check
+    move $t6, $v0        # used for the constructed stack pointer
+
+    # a for loop to push and pop braces   
+    # $t0: True content length.
+    # $t1: Counter which is used to control the for loop
+    # $t5: Number of successfully matched pairs
+    # $t6: Stack pointer
+    # $t7: Address for later check(if there are extra braces in the stack)
+    move $t1, $zero
+    move $t5, $zero
+for:
+    sle $t2, $t1, $t0
+    beqz $t2, end_for
+    lb $t2, content($t1)
+    # $t2 == left braces, push; $t2 == right braces, check, match and pop, else print mismatch msg; for other chars, just ignore.
+    jal charCheck
+    addi $t1, $t1, 1
+    b for
+
+end_for:
+    # check if there are left braces in the stack
+    bne $t6, $t7, extraBraces
+    la $a0, successpre
+    li $v0, 4
+    syscall
+    move $a0, $t5
+    li $v0, 1
+    syscall
+    la $a0, successsuff
+    li $v0, 4
+    syscall
+
+    li $v0, 11
+    li $a0, 10
+    syscall     # Print a new line
+
+    # Exit
+    li $v0, 10
+    syscall
 
 
 badArg:
@@ -44,10 +175,57 @@ badArg:
     li $v0, 4
     syscall
 
+    li $v0, 11
+    li $a0, 10
+    syscall     # Print a new line
+
     # Exit
     li $v0, 10
     syscall
 
+mismatch:
+    # $t2 contains the mismatch charcter and pos number is $t1
+    la $a0, matcherrorpre
+    li $v0, 4
+    syscall
+    move $a0, $t2
+    li $v0, 11
+    syscall
+    la $a0, matcherrorsuff
+    li $v0, 4
+    syscall
+    move $a0, $t1
+    li $v0, 1
+    syscall
+
+    li $v0, 11
+    li $a0, 10
+    syscall     # Print a new line
+
+    # Exit
+    li $v0, 10
+    syscall
+
+extraBraces:
+    # print out the left braces in reverse order, from $t6 to $t7
+    la $a0, extraerror
+    li $v0, 4
+    syscall
+reverseChar:
+    lb $a0, -1($t6)
+    li $v0, 11
+    syscall
+    subi $t6, $t6, 1
+    bne $t6, $t7, reverseChar
+
+    li $v0, 11
+    li $a0, 10
+    syscall     # Print a new line
+
+    # Exit
+    li $v0, 10
+    syscall
+    
 
 checkFirstChar:
     # $t0 contains the first char
@@ -60,7 +238,7 @@ checkFirstChar:
     and $t2, $t2, $t3
 
     or $t1, $t1, $t2
-    move $v0, $t1
+    beqz $t1, badArg
     jr $ra
     
 checkFileLen:
@@ -73,4 +251,45 @@ while:
     addi $t0, $t0, 1
     b while
 while_end:
+    jr $ra
+
+charCheck:
+    # $t2 contains the target char
+    beq $t2, 40, push
+    beq $t2, 91, push
+    beq $t2, 123, push
+    beq $t2, 41, checkParen
+    beq $t2, 93, checkBrack
+    beq $t2, 125, checkBrace
+    # Character which are not braces, brackets or parentheses
+    jr $ra 
+push:
+    sb $t2, 0($t6)
+    addi $t6, $t6, 1
+    jr $ra
+checkParen:
+    lb $t3, -1($t6)
+    beq $t3, 40, pop
+    # mismatch condition, just exit, no need to go the original caller point
+    jal mismatch
+
+checkBrack:
+    lb $t3, -1($t6)
+    beq $t3, 91, pop
+    jal mismatch
+
+checkBrace:
+    lb $t3, -1($t6)
+    beq $t3, 123, pop
+    jal mismatch
+
+pop:
+    subi $t6, $t6, 1
+    addi $t5, $t5, 1
+    jr $ra
+
+printNewLine:
+    li $a0, 10
+    li $v0, 11
+    syscall
     jr $ra
