0aa0e7b100defc00d6a4afefa105e1fa4802afec
Jason Heywood <jasonzhansen@Foxmail.com>
2021-03-09 11:15:20 +0800

    finish bit wise translation

---
+++ CS144/Lab2/wrapping_integers.cc
@@ -0,0 +1,43 @@
+#include "wrapping_integers.hh"
+
+// Dummy implementation of a 32-bit wrapping integer
+
+// For Lab 2, please replace with a real implementation that passes the
+// automated checks run by `make check_lab2`.
+
+template <typename... Targs>
+void DUMMY_CODE(Targs &&... /* unused */) {}
+
+using namespace std;
+
+//! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
+//! \param n The input absolute 64-bit sequence number
+//! \param isn The initial sequence number
+WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
+    // 64 bit will exceed 32 bit limit
+    uint64_t temp = static_cast<uint64_t>(isn.raw_value());
+    temp += n;
+    temp = temp % (1ul << 32);
+    return WrappingInt32{static_cast<uint32_t>(temp)};
+}
+
+//! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
+//! \param n The relative sequence number
+//! \param isn The initial sequence number
+//! \param checkpoint A recent absolute 64-bit sequence number
+//! \returns the 64-bit sequence number that wraps to `n` and is closest to `checkpoint`
+//!
+//! \note Each of the two streams of the TCP connection has its own ISN. One stream
+//! runs from the local TCPSender to the remote TCPReceiver and has one ISN,
+//! and the other stream runs from the remote TCPSender to the local TCPReceiver and
+//! has a different ISN.
+uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
+    uint64_t ans =(checkpoint & 0xFFFFFFFF00000000) + (n.raw_value() - isn.raw_value());
+    if(ans >= checkpoint && ans - (1ul << 32) < ans && ans - checkpoint >= (1ul << 31)){
+        return ans - (1ul << 32);
+    }
+    if(ans < checkpoint && ans + (1ul << 32) > ans && checkpoint - ans > (1ul << 31)){
+        return ans + (1ul << 32);
+    }
+    return ans;
+}
---
+++ lua_tutorial/minusUnsign.cc
@@ -0,0 +1,10 @@
+#include<cstdio>
+#include<cstdint>
+#include<iostream>
+using namespace std;
+
+int main(){
+    uint64_t ans = UINT32_MAX - 0;
+    cout<<ans<<endl;
+    return 0;
+}
